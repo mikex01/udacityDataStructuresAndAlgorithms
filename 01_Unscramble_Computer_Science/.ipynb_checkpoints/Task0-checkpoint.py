a101dc81151d63d798dca560c3d77a79c723b842
mikex01 <miguel.itzep01@gmail.com>
2020-04-11 10:36:45 -0600

    Finished project

--- 01_Unscramble_Computer_Science/.ipynb_checkpoints/Task0-checkpoint.py
+++ 01_Unscramble_Computer_Science/.ipynb_checkpoints/Task0-checkpoint.py
@@ -20,6 +20,6 @@ Print messages:
 """
 
 print("\nFirst record of texts, {0} texts {1} at time {2}" \
-      .format(texts[0][0],texts[0][1],texts[0][2]))
+      .format(texts[0][0], texts[0][1], texts[0][2]))
 print("Last record of calls, {0} calls {1} at time {2}, lasting {3} seconds" \
       .format(calls[-1][0], calls[-1][1], calls[-1][2], calls[-1][3]))
